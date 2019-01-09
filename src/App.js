824b1ca15adc589dd4e84f9bd88f3eb095f02bf8
RachelKolk <rach.m.obrien@gmail.com>
2019-01-09 15:44:44 -0500

    made sample todos array

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import React from 'react';
 
+const todoTasks = [
+  {
+    task: 'Write Thank Yous',
+    id: 1,
+    completed: false
+  },
+  {
+    task: 'Laundry',
+    id: 2,
+    completed: false
+  },
+  {
+    task: 'Clean Living Room',
+    id: 3,
+    completed: false
+  }
+];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
